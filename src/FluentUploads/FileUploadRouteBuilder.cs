a5dbbb288bdecfb2df1907c1b28767978885d217
joshdixon <6028199+joshdixon@users.noreply.github.com>
2023-12-31 15:02:45 +1030

    updated router builder

--- src/FluentUploads/FileUploadRouteBuilder.cs
+++ src/FluentUploads/FileUploadRouteBuilder.cs
@@ -1,32 +1,25 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace FluentUploads;
 
-
-public class FileUploadRouteBuilder<TMetadata>
+public class FileUploadRouteBuilder<TMetadata> : IEndpointConventionBuilder
 {
     private readonly IEndpointRouteBuilder _routeBuilder;
-    private readonly string _pattern;
-    private Func<HttpContext, TMetadata, Task>? _completionFunc;
-    private readonly Delegate _handler;
+    private readonly IEndpointConventionBuilder _conventionBuilder;
+    private Func<TMetadata, IServiceProvider, Task>? _completionFunc;
 
     public FileUploadRouteBuilder(IEndpointRouteBuilder routeBuilder, string pattern, Delegate handler)
     {
         _routeBuilder = routeBuilder;
-        _pattern = pattern;
-        _handler = handler;
+        _conventionBuilder = routeBuilder.MapPost(pattern, handler);;
     }
 
-    public FileUploadRouteBuilder<TMetadata> OnUploadComplete(Func<HttpContext, TMetadata, Task> completionFunc)
+    public FileUploadRouteBuilder<TMetadata> WithCompletionHandler(Func<TMetadata, IServiceProvider, Task> completionFunc)
     {
         _completionFunc = completionFunc;
         return this;
     }
 
-    public void Build()
-    {
-        _routeBuilder.MapPost(_pattern, _handler);
-    }
+    public void Add(Action<EndpointBuilder> convention) => _conventionBuilder.Add(convention);
 }
